62089c9f348b8cb469d409454e1a9aca2b980d40
Zac Crawford <zwcrawford@gmail.com>
2018-12-11 14:36:47 -0600

    added code to domBuilder.js, created domComponents.js

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -10,7 +10,7 @@ const domBuilder =  {
       // Picking the id "form-container"
       let userEntryFields = document.getElementById("form-container");
 
-      // The search results and itinerary sections below may need to be in a different function...but I have left them visible for now.
+      // The search results and itinerary sections below may need to be in a different function...I have left them commented out for now.
       userEntryFields.innerHTML = `
         <form>
           <h1>Welcome to Nashville</h1>
@@ -26,22 +26,53 @@ const domBuilder =  {
                 <input type="text" name="meetups" id="meet_search" placeholder="Search Meetups by Topic"> 
                 <button type="button" id="meet_search">Search Meetups</button>
               </br>
-                <input type="text" name="concerts" id="show_search" placeholder="Search Meetups by Topic"> 
+                <input type="text" name="concerts" id="show_search" placeholder="Search Concerts by Genre"> 
                 <button type="button" id="show_search">Search Live Shows</button>
-            </section>
-          </fieldset>
-          <fieldset>
-            <legend> Search Results: </legend>
-              <section>
-              </section>
-          </fieldset>
-          <fieldset>
-            <legend> My Itinerary: </legend>
-              <section>
-              </section>
-          </fieldset>
-        </form>`
-        return userEntryFields;
-    }
+            </section>    
+        </form>`;  
+    },
+    appendResourceContainers () {
+      // Trying to define one large container to horl results and itinerary containers.
+      let mainContainer = document.getElementById("display-container");
+
+      // Search container to hold results from inputs.
+      const searchContainer = domComponents.createDomElement("article", null, "search-container");
+      // Append the "Search Results" string"
+      mainContainer.appendChild(domComponents.createDomElement("h3", "Search Results:", null));
+      // Append search container to main container
+      mainContainer.appendChild(searchContainer);
+
+      // Append itinerary to searcjh if "Saved", *** I think this belongs elsewhere
+      //searchContainer.appendChild(itineraryContainer);
+      
+    }//,
+//     appendAllResources (fragmentsObj) {
+//       let articlesContainer = document.querySelector("#articles-container");
+//       let videosContainer = document.querySelector("#videos-container");
+//       articlesContainer.innerHTML = "";
+//       videosContainer.innerHTML = "";
+//       articlesContainer.appendChild(fragmentsObj.articles);
+//       videosContainer.appendChild(fragmentsObj.videos);
+//     },
+//     appendSearchContainer () {
+//       let divContainer = document.querySelector("#display-container");
+  
+//       const searchContainer = domComponents.createDomElement("article", null, "search-container");
+  
+//       searchContainer.appendChild(domComponents.createDomElement("label", "Search", null));
+//       let searchInput = document.createElement("input");
+//       searchInput.setAttribute("type", "text");
+//       searchInput.setAttribute("name", "searchinput");
+//       searchContainer.appendChild(searchInput);
+  
+//       let searchButton = domComponents.createDomElement("button", "Search", null);
+//       searchButton.setAttribute("id", "search-button");
+//       searchButton.addEventListener("click", eventListeners.handleSearchButton);
+//       searchContainer.appendChild(searchButton);
+  
+//       divContainer.appendChild(searchContainer);
+//     }
 };
+//*** move these to main.js for the calls only!!! ***
 domBuilder.searchFormBuilder();
+domBuilder.appendResourceContainers();
